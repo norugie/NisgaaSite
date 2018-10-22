fd8a8c76a70c6ea96b06a6845134af771f88b143
Rugie Ann Barrameda <norugie015@gmail.com>
2018-10-22 15:18:29 -0700

    credit stuff

--- components/page_sidebar.php
+++ components/page_sidebar.php
@@ -363,11 +363,11 @@
     <!-- #Menu -->
     <!-- Footer -->
     <div class="legal">
-        <div class="copyright">
-            &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
-        </div>
         <div class="version">
-            <b>Version: </b> 1.0.5
+            &copy; <b>2018 SD92-CMS</b> v1.0
+        </div>
+        <div class="copyright">
+            &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>
         </div>
     </div>
     <!-- #Footer -->
