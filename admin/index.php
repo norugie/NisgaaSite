940525847e69da241944e87a6196fafcb1dba9cc
Rugie Ann Barrameda <norugie015@gmail.com>
2018-10-22 15:03:01 -0700

    initial changes

---
+++ admin/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Page Header -->
+    <?php require '../components/page_head.php'; ?>
+    <!-- #END# Page Header -->
+</head>
+
+<body class="theme-blue-grey">
+    
+    <!-- Page Loader -->
+    <?php require '../components/page_loader.php'; ?>
+    <!-- #END# Page Loader -->
+
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    
+    <!-- Search Bar -->
+    <?php require '../components/page_search.php'; ?>
+    <!-- #END# Search Bar -->
+    
+    <!-- Top Bar -->
+    <?php require '../components/page_navbar.php'; ?>
+    <!-- #Top Bar -->
+    
+    <section>
+        <!-- Left Sidebar -->
+        <?php require '../components/page_sidebar.php'; ?>
+        <!-- #END# Left Sidebar -->
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>DASHBOARD</h2>
+            </div>
+
+            <!-- Page Content -->
+
+            <!-- #END# Page Content -->
+
+        </div>
+    </section>
+
+
+    <!-- Page Footer -->
+    <?php require '../components/page_foot.php'; ?>
+    <!-- #END# Page Footer -->
+
+</body>
+
+</html>
---
+++ components/page_head.php
@@ -0,0 +1,23 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<title>Dasboard | SD92 - Nisga'a</title>
+<!-- Favicon-->
+<link rel="icon" href="../nisgaa-icon.png" type="image/x-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+<!-- Bootstrap Core Css -->
+<link href="../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+<!-- Waves Effect Css -->
+<link href="../plugins/node-waves/waves.css" rel="stylesheet" />
+
+<!-- Animation Css -->
+<link href="../plugins/animate-css/animate.css" rel="stylesheet" />
+
+<!-- Custom Css -->
+<link href="../css/style.css" rel="stylesheet">
+<link href="../css/themes/theme-blue-grey.css" rel="stylesheet" />
