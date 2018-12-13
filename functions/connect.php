0453a0484309c23113176c3b318ebff42fbbea5f
Rugie Ann Barrameda <norugie015@gmail.com>
2018-12-12 22:02:51 -0800

    database changes

--- functions/connect.php
+++ functions/connect.php
@@ -3,7 +3,7 @@
 	class Database {
 		public $con;
 		public function __construct(){
-			$this->con = mysqli_connect("localhost", "root", "", "nisgaa_db");
+			$this->con = mysqli_connect("phpmyadmin.me", "root", "", "nisgaa_db");
 			if (!$this->con) {
 				echo "Error in Connecting: " . mysqli_connect_error();
 			}
