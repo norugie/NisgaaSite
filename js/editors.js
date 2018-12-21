2369c6be5e441b9cbcd27c853854259eee4001a8
Rugie Ann Barrameda <norugie015@gmail.com>
2018-12-20 19:08:36 -0800

    upload image to tinymce

--- js/editors.js
+++ js/editors.js
@@ -12,6 +12,39 @@ $(function () {
         ],
         toolbar1: 'insertfile undo redo | styleselect | fontsizeselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
         toolbar2: 'print preview | forecolor backcolor emoticons',
-        image_advtab: true
+        image_advtab: true,
+        images_upload_url: '../functions/upload.php',
+        
+        // Override default upload handler to simulate successful upload
+        images_upload_handler: function (blobInfo, success, failure) {
+            var xhr, formData;
+        
+            xhr = new XMLHttpRequest();
+            xhr.withCredentials = false;
+            xhr.open('POST', '../functions/upload.php');
+        
+            xhr.onload = function() {
+                var json;
+            
+                if (xhr.status != 200) {
+                    failure('HTTP Error: ' + xhr.status);
+                    return;
+                }
+            
+                json = JSON.parse(xhr.responseText);
+            
+                if (!json || typeof json.location != 'string') {
+                    failure('Invalid JSON: ' + xhr.responseText);
+                    return;
+                }
+            
+                success(json.location);
+            };
+        
+            formData = new FormData();
+            formData.append('file', blobInfo.blob(), blobInfo.filename());
+        
+            xhr.send(formData);
+        },
     });
 });
