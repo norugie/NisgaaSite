551dfa201b5daecc60a710758bbbd342323f502b
Rugie Ann Barrameda <norugie015@gmail.com>
2018-12-31 13:21:53 -0800

    changes with format

--- components/page_foot.php
+++ components/page_foot.php
@@ -35,53 +35,5 @@
 <script src="../js/admin.js"></script>
 <script src="../js/dialogs.js"></script>
 <script src="../js/edit.js"></script>
+<script src="../js/custom.js"></script>
 
-
-<!-- Custom JS DataTable -->
-<script>
-	$('.js-basic-example').DataTable({
-    	"bSort": false,
-    	"lengthChange": false,
-    	"iDisplayLength": 5
-	});
-</script>
-
-<!-- Custom JS Bootstrap DatePicker -->
-<script>
-$('#bs_datepicker_range_container_new').datepicker({
-	autoclose: true,
-	container: '#bs_datepicker_range_container_new',
-	todayHighlight: true,
-	startDate: '0d',
-	format: 'dd M yyyy'
-});
-
-$('#bs_datepicker_range_container_edit').datepicker({
-	autoclose: true,
-	container: '#bs_datepicker_range_container_edit',
-	todayHighlight: true,
-	startDate: '0d',
-	format: 'dd M yyyy'
-});
-
-$('#bs_datepicker_range_container_event').datepicker({
-	autoclose: true,
-	container: '#bs_datepicker_range_container_event',
-	todayHighlight: true,
-	startDate: '0d',
-	format: 'dd M yyyy'
-});
-
-</script>
-
-<!-- Custom JQuery Validator -->
-<script>
-    $('.form_validate').validate({
-        unhighlight: function (input) {
-            $(input).parents('.form-line').removeClass('error');
-        },
-        errorPlacement: function (error, element) {
-            $(element).parents('.form-group').append(error);
-        }
-    });
-</script>
---
+++ js/custom.js
@@ -0,0 +1,41 @@
+// Custom JS DataTable
+$('.js-basic-example').DataTable({
+    "bSort": false,
+    "lengthChange": false,
+    "iDisplayLength": 5
+});
+
+// Custom JS Bootstrap DatePicker
+$('#bs_datepicker_range_container_new').datepicker({
+	autoclose: true,
+	container: '#bs_datepicker_range_container_new',
+	todayHighlight: true,
+	startDate: '0d',
+	format: 'dd M yyyy'
+});
+
+$('#bs_datepicker_range_container_edit').datepicker({
+	autoclose: true,
+	container: '#bs_datepicker_range_container_edit',
+	todayHighlight: true,
+	startDate: '0d',
+	format: 'dd M yyyy'
+});
+
+$('#bs_datepicker_range_container_event').datepicker({
+	autoclose: true,
+	container: '#bs_datepicker_range_container_event',
+	todayHighlight: true,
+	startDate: '0d',
+	format: 'dd M yyyy'
+});
+
+// Custom JQuery Validator
+$('.form_validate').validate({
+    unhighlight: function (input) {
+        $(input).parents('.form-line').removeClass('error');
+    },
+    errorPlacement: function (error, element) {
+        $(element).parents('.form-group').append(error);
+    }
+});
