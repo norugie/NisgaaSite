30960a6131be5359606ba9669510251063e9de64
Rugie Ann Barrameda <norugie015@gmail.com>
2019-01-02 20:20:34 -0800

    added bootstrap v4; bootstrap 3.3.6 will stay for backend template

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
     <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/bootstrap-v4/css/bootstrap.min.css">
     <!-- Font Awesome CSS-->
     <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
     <!-- Google fonts - Roboto-->
@@ -311,149 +311,7 @@
         </div>
         <!-- *** JUMBOTRON END ***-->
       </section>
-      <section class="bar bg-gray no-mb padding-big text-md-center">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <h2 class="text-uppercase">Everything you need</h2>
-              <p class="lead mb-small">40+ prepared HTML templates</p>
-              <p class="mb-small">We have prepared literally <strong>everything you would possibly need building corporate, e-commerce or portfolio website. If you still miss something, let us know and we will try to include it in theme's future updates.</strong></p>
-              <p><a href="#" class="btn btn-template-main">Read more</a></p>
-            </div>
-            <div class="col-md-6 text-center"><img src="img/template-easy-customize.png" alt="" class="img-fluid"></div>
-          </div>
-        </div>
-      </section>
-      <section class="bar no-mb color-white padding-big text-md-center bg-primary">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 text-center"><img src="img/template-easy-code.png" alt="" class="img-fluid"></div>
-            <div class="col-md-6">
-              <h2 class="text-uppercase">Clean, valid and documented code & support</h2>
-              <p class="lead mb-small">We made our best to help you</p>
-              <p class="mb-small">Everything in this template is properly named and categorized. In case you don't find in the documentation answer to your question, our support is here to help!</p>
-              <p><a href="#" class="btn btn-template-outlined-white">Read more</a></p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="bar bg-gray no-mb padding-big text-center-sm">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <h2 class="text-uppercase">Easy to customize</h2>
-              <p class="mb-small">We have made this template with focus on future client customization. You get <strong>40+ prepared HTML templates</strong>, many <strong>reusable components</strong> and <strong>LESS files</strong> for even easier template customization.</p>
-              <p><a href="#" class="btn btn-template-main">Read more</a></p>
-            </div>
-            <div class="col-md-6 text-center"><img src="img/template-easy-customize.png" alt="" class="img-fluid"></div>
-          </div>
-        </div>
-      </section>
-      <section id="packages" class="bar no-mb">
-        <div data-animate="fadeInUp" class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="heading text-center">
-                <h2>Packages we offer</h2>
-              </div>
-              <p class="lead">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-              <!-- Packages Start-->
-              <div class="row packages">
-                <div class="col-md-3">
-                  <div class="package">
-                    <div class="package-header light-gray">
-                      <h5>Basic</h5>
-                    </div>
-                    <div class="price">
-                      <div class="price-container d-flex align-items-end justify-content-center">
-                        <h4 class="h1"><span class="currency">$</span>7</h4><span class="period">/ month</span>
-                      </div>
-                    </div>
-                    <ul class="list-unstyled">
-                      <li><i class="fa fa-check"></i>100GB Storage</li>
-                      <li><i class="fa fa-check"></i>Own config file</li>
-                      <li><i class="fa fa-times"></i>Sharing Tools</li>
-                      <li><i class="fa fa-times"></i>Auto Backup</li>
-                      <li><i class="fa fa-times"></i>Extended Security</li>
-                    </ul><a href="#" class="btn btn-template-outlined">Sign Up</a>
-                  </div>
-                </div>
-                <!-- / END FIRST PACKAGE-->
-                <div class="col-md-3">
-                  <div class="package">
-                    <div class="package-header light-gray">
-                      <h5>Standard</h5>
-                    </div>
-                    <div class="price">
-                      <div class="price-container d-flex align-items-end justify-content-center">
-                        <h4 class="h1"><span class="currency">$</span>21</h4><span class="period">/ month</span>
-                      </div>
-                    </div>
-                    <ul class="list-unstyled">
-                      <li><i class="fa fa-check"></i>100GB Storage</li>
-                      <li><i class="fa fa-check"></i>Own config file</li>
-                      <li><i class="fa fa-times"></i>Sharing Tools</li>
-                      <li><i class="fa fa-times"></i>Auto Backup</li>
-                      <li><i class="fa fa-times"></i>Extended Security</li>
-                    </ul><a href="#" class="btn btn-template-outlined">Sign Up</a>
-                  </div>
-                </div>
-                <div class="col-md-3">
-                  <div class="best-value">
-                    <div class="package">
-                      <div class="package-header bg-primary color-white">
-                        <div class="content">
-                          <h5>Premium</h5>
-                          <div class="meta-text">Best Value</div>
-                        </div>
-                      </div>
-                      <div class="price-container d-flex align-items-end justify-content-center">
-                        <h4 class="h1"><span class="currency">$</span>53</h4><span class="period">/ month</span>
-                      </div>
-                      <ul class="list-unstyled">
-                        <li><i class="fa fa-check"></i>100GB Storage</li>
-                        <li><i class="fa fa-check"></i>Own config file</li>
-                        <li><i class="fa fa-times"></i>Sharing Tools</li>
-                        <li><i class="fa fa-times"></i>Auto Backup</li>
-                        <li><i class="fa fa-times"></i>Extended Security</li>
-                      </ul><a href="#" class="btn btn-template-outlined">Sign Up</a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-3">
-                  <div class="package">
-                    <div class="package-header light-gray">
-                      <h5>Ultimate</h5>
-                    </div>
-                    <div class="price-container d-flex align-items-end justify-content-center">
-                      <h4 class="h1"><span class="currency">$</span>99</h4><span class="period">/ month</span>
-                    </div>
-                    <ul class="list-unstyled">
-                      <li><i class="fa fa-check"></i>100GB Storage</li>
-                      <li><i class="fa fa-check"></i>Own config file</li>
-                      <li><i class="fa fa-times"></i>Sharing Tools</li>
-                      <li><i class="fa fa-times"></i>Auto Backup</li>
-                      <li><i class="fa fa-times"></i>Extended Security</li>
-                    </ul><a href="#" class="btn btn-template-outlined">Sign Up</a>
-                  </div>
-                </div>
-              </div>
-              <!-- Packages End-->
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- GET IT-->
-      <div class="get-it">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 text-center p-3">
-              <h3>Do you want cool website like this one?</h3>
-            </div>
-            <div class="col-lg-4 text-center p-3">   <a href="#" class="btn btn-template-outlined-white">Buy this template now</a></div>
-          </div>
-        </div>
-      </div>
+
       <!-- FOOTER -->
       <footer class="main-footer">
         <div class="container">
@@ -518,7 +376,7 @@
           <div class="container">
             <div class="row">
               <div class="col-lg-4 text-center-md">
-                <p>&copy; 2018. Your company / name goes here</p>
+                <p>&copy; 2018. SD92 (Nisga'a)</p>
               </div>
               <div class="col-lg-8 text-right text-center-md">
                 <p>Template design by <a href="https://bootstrapious.com/free-templates">Bootstrapious Templates </a></p>
@@ -531,7 +389,7 @@
     </div>
     <!-- Javascript files-->
     <script src="plugins/jquery/jquery.min.js"></script>
-    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="plugins/bootstrap-v4/js/bootstrap.min.js"></script>
     <script src="plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
   </body>
 </html>
