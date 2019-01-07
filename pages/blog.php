c043e9d86d88ecc10b9391a18bcf8401da278270
Rugie Ann Barrameda <norugie015@gmail.com>
2019-01-07 13:57:20 -0800

    blog page template

--- pages/blog.php
+++ pages/blog.php
@@ -1,84 +1,109 @@
-
+<!-- BLOG LIST -->
 
 <div id="blog-listing-medium" class="col-md-9">
     <section class="post">
         <div class="row">
             <div class="col-md-3">
-                <div class="image"><a href="blog-post.html"><img src="img/blog-medium.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
+                <div class="image"><a href="blog-post.html"><img src="img/portfolio-1.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
             </div>
             <div class="col-md-9">
-                <h2 class="h3 mt-0"><a href="post.htmls">Fashion now</a></h2>
+                <h2 class="h3 mt-0"><a href="post.htmls">Sample Title 1</a></h2>
                 <div class="d-flex flex-wrap justify-content-between text-xs">
                     <p class="author-category">By John Snow
                     <p class="date-comments"><a href="blog-post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
                 </div>
                 <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="blog-post.html">Read More</a>
+                <a href="blog-post.html">Read More →</a>
             </div>
         </div>
     </section>
     <section class="post">
         <div class="row">
             <div class="col-md-3">
-                <div class="image"><a href="blog-post.html"><img src="img/blog-medium.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
+                <div class="image"><a href="blog-post.html"><img src="img/portfolio-2.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
             </div>
             <div class="col-md-9">
-                <h2 class="h3 mt-0"><a href="post.htmls">Fashion now</a></h2>
+                <h2 class="h3 mt-0"><a href="post.htmls">Sample Title 2</a></h2>
                 <div class="d-flex flex-wrap justify-content-between text-xs">
                     <p class="author-category">By John Snow
                     <p class="date-comments"><a href="blog-post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
                 </div>
                 <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="blog-post.html">Read More</a>
+                <a href="blog-post.html">Read More →</a>
             </div>
         </div>
     </section>
     <section class="post">
         <div class="row">
             <div class="col-md-3">
-                <div class="image"><a href="blog-post.html"><img src="img/blog-medium.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
+                <div class="image"><a href="blog-post.html"><img src="img/portfolio-3.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
             </div>
             <div class="col-md-9">
-                <h2 class="h3 mt-0"><a href="post.htmls">Fashion now</a></h2>
+                <h2 class="h3 mt-0"><a href="post.htmls">Sample Title 3</a></h2>
                 <div class="d-flex flex-wrap justify-content-between text-xs">
                     <p class="author-category">By John Snow
                     <p class="date-comments"><a href="blog-post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
                 </div>
                 <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="blog-post.html">Read More</a>
+                <a href="blog-post.html">Read More →</a>
             </div>
         </div>
     </section>
     <section class="post">
         <div class="row">
             <div class="col-md-3">
-                <div class="image"><a href="blog-post.html"><img src="img/blog-medium.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
+                <div class="image"><a href="blog-post.html"><img src="img/portfolio-4.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
             </div>
             <div class="col-md-9">
-                <h2 class="h3 mt-0"><a href="post.htmls">Fashion now</a></h2>
+                <h2 class="h3 mt-0"><a href="post.htmls">Sample Title 4</a></h2>
                 <div class="d-flex flex-wrap justify-content-between text-xs">
                     <p class="author-category">By John Snow
                     <p class="date-comments"><a href="blog-post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
                 </div>
                 <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="blog-post.html">Read More</a>
+                <a href="blog-post.html">Read More →</a>
             </div>
         </div>
     </section>
     <section class="post">
         <div class="row">
             <div class="col-md-3">
-                <div class="image"><a href="blog-post.html"><img src="img/blog-medium.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
+                <div class="image"><a href="blog-post.html"><img src="img/portfolio-5.jpg" alt="Example blog post alt" class="img-fluid"></a></div>
             </div>
             <div class="col-md-9">
-                <h2 class="h3 mt-0"><a href="post.htmls">Fashion now</a></h2>
+                <h2 class="h3 mt-0"><a href="post.htmls">Sample Title 5</a></h2>
                 <div class="d-flex flex-wrap justify-content-between text-xs">
                     <p class="author-category">By John Snow
                     <p class="date-comments"><a href="blog-post.html"><i class="fa fa-calendar-o"></i> June 20, 2013</a>
                 </div>
                 <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                <a href="blog-post.html">Read More</a>
+                <a href="blog-post.html">Read More →</a>
             </div>
         </div>
     </section>
+
+
+    <!-- PAGINATION -->
+
+        
+    <div class="pager list-unstyled d-flex align-items-center pagination pagination-lg">
+        <p class="page-item"><a href="#" class="page-link">First Page</a></p>
+        <p class="page-item active"><a href="#" class="page-link">1</a></p>
+        <p class="page-item"><a href="#" class="page-link">2</a></p>
+        <p class="page-item"><a href="#" class="page-link">3</a></p>
+        <p class="page-item"><a href="#" class="page-link">4</a></p>
+        <p class="page-item"><a href="#" class="page-link">5</a></p>
+        <p class="page-item"><a href="#" class="page-link">Last Page</a></p>
+    </div>
+
+
+    <!-- <ul class="pager list-unstyled d-flex align-items-center justify-content-between mb-0 pagination pagination-lg">
+        <li class="previous"><a href="#" class="btn btn-template-outlined">← Older</a></li>
+        <li class="page-item"><a href="#">1</a></li>
+        <li class="page-item"><a href="#">2</a></li>
+        <li class="page-item"><a href="#">3</a></li>
+        <li class="page-item"><a href="#">4</a></li>
+        <li class="page-item"><a href="#">5</a></li>
+        <li class="next"><a href="#" class="btn btn-template-outlined">Newer →</a></li>
+    </ul> -->
 </div>
