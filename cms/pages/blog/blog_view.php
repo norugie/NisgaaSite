6e86f409ccf0c1e576fe522c1b8c2e9700a56c17
Rugie Ann Barrameda <norugie015@gmail.com>
2019-01-15 14:30:06 -0800

    added edit link from view pages

--- cms/pages/blog/blog_view.php
+++ cms/pages/blog/blog_view.php
@@ -7,7 +7,7 @@
         <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
             <center>
                 <button type="button" class="btn bg-blue waves-effect" style="display: inline-block; margin-top: 4px;" onclick="window.location.href='post.php?tab=post&page=blog'"><i class="material-icons">list</i></button>
-                <button type="button" class="btn bg-green waves-effect" style="display: inline-block; margin-top: 4px; margin-left: 5px;" onclick="window.location.href='post.php?tab=post&page=blog'"><i class="material-icons">mode_edit</i></button>
+                <button type="button" class="btn bg-green waves-effect" style="display: inline-block; margin-top: 4px; margin-left: 5px;" onclick="window.location.href='post.php?tab=post&page=blog&blog_option=modify&modify=details&post_id=<?php echo $p['id']; ?>'"><i class="material-icons">mode_edit</i></button>
                 <button type="button" class="btn bg-red waves-effect" style="display: inline-block; margin-top: 4px; margin-left: 5px;" data-type="delete-post" data-id="<?php echo $post_info['id']; ?>" data-name="<?php echo $post_info['post_id']; ?>" data-event="<?php echo $event_indicator; ?>" onclick="alertDesign(this);"><i class="material-icons">delete</i></button>
             </center>
         </div>
