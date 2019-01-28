2c8e6b4c2b29b76348428e5030af800ed5adbbf0
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-01-28 12:40:58 -0800

    about and contacts info

---
+++ cms/pages/about.php
@@ -0,0 +1,72 @@
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 text-xs-sm-center">
+                        <h4>ABOUT US</h4>      
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                        <center>
+                            <button type="button" class="btn bg-green waves-effect" style="display: inline-block;"><i class="material-icons">mode_edit</i><span>MODIFY</span></button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 text-xs-sm-center">
+                        <h4>PROGRAMS</h4>      
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                        <center>
+                            <button type="button" class="btn bg-green waves-effect" style="display: inline-block;"><i class="material-icons">mode_edit</i><span>MODIFY</span></button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-xs-sm-center">
+                        <h4>VISION FOR EDUCATION</h4>      
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
