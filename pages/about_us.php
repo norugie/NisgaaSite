57bff79f4543522c54531d05c21612885164659f
Rugie Ann Barrameda <norugie015@gmail.com>
2019-01-31 14:23:34 -0800

    about and programs list on site

--- pages/about_us.php
+++ pages/about_us.php
@@ -1,3 +1,7 @@
+<?php 
+    $about = $site->aboutList($database, 2); 
+    $programs = $site->programList($database, 2);
+?>
 <div class="col-md-9">
     <!-- ABOUT US CONTENT -->
     <section>
@@ -6,7 +10,7 @@
                 <div class="heading">
                     <h2>School District No. 92 (Nisga'a)</h2>
                 </div>
-                <p class="lead">The Nisga’a School District is situated in the Nass Valley and students are primarily of Nisga’a descent. Our district is unique in that it’s small and students are members of one language and cultural group – the Nisga’a Nation. From the Nass Valley it is an easy drive to a ski hill, numerous hotsprings, many world-class fishing spots and Terrace, a regional centre that includes many amenities. For more information about the Nisga’a Nation and the Nass Valley visit the <a href="http://www.nisgaanation.ca/">Nisga’a Lisims Government website</a>.</p>
+                <p class="lead"><?php echo $about['web_desc']; ?></p>
             </div>
         </div>
     </section>
@@ -18,7 +22,7 @@
                 <div class="heading">
                     <h2>PROGRAMS</h2>
                 </div>
-                <p class="lead">The Nisga’a School District takes pride in preparing our students to become fluent Nisga’a speakers, successful readers and writers and good all round citizens. We enjoy great Elementary and Secondary Band Programs, many opportunities in the Fine and Practical Arts and lots of sports success stories. Our schools are small, energetic and intimate and they reflect the welcoming nature of the Nisga’a people and culture.</p>
+                <p class="lead"><?php echo $programs['web_desc']; ?></p>
             </div>
         </div>
     </section>
