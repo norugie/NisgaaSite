3f6c83a1686b20fcb12582ed30ef06ad2bee8d28
Rugie Ann Barrameda <norugie015@gmail.com>
2019-02-05 14:37:39 -0800

    page information setup for modify

--- cms/pages/curdept.php
+++ cms/pages/curdept.php
@@ -1,26 +1,13 @@
 <div class="row clearfix">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="card">
-            <div class="header">
-                <div class="row clearfix">
-                    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 text-xs-sm-center">
-                        <h4>PAGE INFORMATION</h4>      
-                    </div>
-                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-                        <center>
-                            <button type="button" data-toggle="modal" data-target="#edit-school-modal" class="btn bg-green waves-effect" style="display: inline-block;" 
-                            data-values='' onclick="editPage(this);"><i class="material-icons">edit</i><span>MODIFY</span></button>
-                        </center>
-                    </div>
-                </div>
-            </div>
-            <div class="body">
-                <div class="row clearfix">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        
-                    </div>
-                </div>
-            </div>
+            <?php
+                if(!isset($_GET['option']) || empty($_GET['option'])){
+                    require 'curdept/curdept_view.php';
+                } else {
+                    require 'curdept/curdept_modify.php';
+                }
+            ?>
         </div>
     </div>
 </div>
---
+++ cms/pages/curdept/curdept_modify.php
@@ -0,0 +1,32 @@
+<div class="header">
+    <div class="row clearfix">
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 text-xs-sm-center">
+            <h4>MODIFY PAGE INFORMATION</h4>      
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <center>
+                <button type="button" class="btn bg-blue waves-effect" style="display: inline-block;" onclick="window.location.href='interaction.php?tab=web&subtab=curriculum&page=<?php echo $_GET['page']; ?>'">PAGE VIEW</button>
+            </center>
+        </div>
+    </div>     
+</div>
+<div class="body">
+    <p class="font-12"><i><b>Note:</b> Fields marked with an asterisk are required</i></p><br>
+    <!-- Inline Layout -->
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+            <form class="edit_form_validate" action="../functions/interaction.php?interaction=true&editPageInformation=true" method="POST">
+
+                <div class="row clearfix">
+                    <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12" style="float: right;">
+                        <button type="submit" class="btn bg-blue-grey btn-block btn-lg waves-effect">SAVE</button>  
+                    </div>
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+    <!-- #END# Inline Layout -->
+</div>
