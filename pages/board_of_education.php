c24be6f920d4458381c68b616adb62fb1fcfa05a
Rugie Ann Barrameda <norugie015@gmail.com>
2019-02-14 10:02:34 -0800

    site boe display template

---
+++ pages/board_of_education.php
@@ -0,0 +1,70 @@
+<div class="col-md-9">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row text-center">
+                <div class="col-md-6">
+                    <div data-animate="fadeInUp" class="team-member">
+                        <div class="image"><a href="team-member.html"><img src="images/contacts/user.png" alt="" class="img-fluid rounded-circle" style="max-width: 300px;"></a></div>
+                        <h3><a href="team-member.html">Han Solo</a></h3>
+                        <p class="role role-main">Founder</p>
+                        <p class="role">New Aiyansh</p>
+                        <p class="role role-contact">test@nisgaa.bc.ca | +1-250-123-4567</p>
+                        <div class="text">
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div data-animate="fadeInUp" class="team-member">
+                        <div class="image"><a href="team-member.html"><img src="images/contacts/user.png" alt="" class="img-fluid rounded-circle" style="max-width: 300px;"></a></div>
+                        <h3><a href="team-member.html">Han Solo</a></h3>
+                        <p class="role role-main">Founder</p>
+                        <p class="role">New Aiyansh</p>
+                        <p class="role role-contact">test@nisgaa.bc.ca | +1-250-123-4567</p>
+                        <div class="text">
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <div data-animate="fadeInUp" class="team-member">
+                        <div class="image"><a href="team-member.html"><img src="images/contacts/user.png" alt="" class="img-fluid rounded-circle" style="max-width: 300px;"></a></div>
+                        <h3><a href="team-member.html">Han Solo</a></h3>
+                        <p class="role role-main">Founder</p>
+                        <p class="role">New Aiyansh</p>
+                        <p class="role role-contact">test@nisgaa.bc.ca | +1-250-123-4567</p>
+                        <div class="text">
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div data-animate="fadeInUp" class="team-member">
+                        <div class="image"><a href="team-member.html"><img src="images/contacts/user.png" alt="" class="img-fluid rounded-circle" style="max-width: 300px;"></a></div>
+                        <h3><a href="team-member.html">Han Solo</a></h3>
+                        <p class="role role-main">Founder</p>
+                        <p class="role">New Aiyansh</p>
+                        <p class="role role-contact">test@nisgaa.bc.ca | +1-250-123-4567</p>
+                        <div class="text">
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div data-animate="fadeInUp" class="team-member">
+                        <div class="image"><a href="team-member.html"><img src="images/contacts/user.png" alt="" class="img-fluid rounded-circle" style="max-width: 300px;"></a></div>
+                        <h3><a href="team-member.html">Han Solo</a></h3>
+                        <p class="role role-main">Founder</p>
+                        <p class="role">New Aiyansh</p>
+                        <p class="role role-contact">test@nisgaa.bc.ca | +1-250-123-4567</p>
+                        <div class="text">
+                            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
