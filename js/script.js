24c39ffa1286c9959e5bad7ab85edb302ff8b175
Rugie Ann Barrameda <norugie015@gmail.com>
2019-03-04 09:28:43 -0800

    search full screen page template

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ $(document).ready(function(){
 $(document).ready(function(){
     setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 50);
 });
+
+// Search
+$(function () {
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+});
