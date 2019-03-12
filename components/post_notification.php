8aebd6a9f12cc3ba83d48179ff7a9c7f5f9f274c
Rugie Ann Barrameda <norugie015@gmail.com>
2019-03-12 12:12:20 -0700

    modify media categories functionality

--- components/post_notification.php
+++ components/post_notification.php
@@ -107,6 +107,13 @@
 </div>
 <?php } ?>
 
+<?php if(isset($_GET['editMediaCategories']) && $_SESSION['alert'] == 'alerted'){ ?>
+<div class="alert bg-green alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    A media post has been <b>modified</b> successfully!
+</div>
+<?php } ?>
+
 <?php if(isset($_GET['mediaDisabled']) && $_SESSION['alert'] == 'alerted'){ ?>
 <div class="alert bg-green alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
