0542bf7ab4f88f26a2e05dcb1699d336054d81c8
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-05-07 14:42:32 -0700

    boe revision

---
+++ components/modals/site_boe.php
@@ -0,0 +1,32 @@
+<div id="boe-modal" tabindex="-1" role="dialog" aria-labelledby="boe-modalLabel" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 id="boe-modalLabel" class="modal-title">Trustee Information</h4>
+                <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <center><img src="" id="trustee-img" class="img-fluid rounded-circle" alt="BOE Trustee Image"></center>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="heading">
+                            <h3 id="trustee-name"></h3>
+                            <p class="lead" style="margin:0px !important;"><strong id="trustee-position"></strong></p>
+                            <p class="lead" id="trustee-place" style="margin:0px !important;"></p>
+                            <p class="lead" style="margin:0px !important;"><strong>Email Address: </strong><span id="trustee-email"></span></p>
+                            <p class="lead" style="margin:0px !important;"><strong>Phone Number: </strong><span id="trustee-phone"></span></p>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="lead" id="trustee-desc"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- pages/board_of_education.php
+++ pages/board_of_education.php
@@ -56,13 +56,36 @@
 
             <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut scelerisque quam, tincidunt tempor arcu. Nullam sed est vel erat molestie molestie. Integer lacus orci, tempor a finibus non, commodo quis ante. Duis id posuere sem. Phasellus non rhoncus tellus, vel tincidunt quam.</p>
         </div>
+        <?php require 'components/modals/site_boe.php'; ?>
         <div class="col-md-12 text-center">
             <div class="box-image-text">
                 <div class="image"><img src="images/contacts/boe-banner.jpg" alt="BOE Banner" class="img-fluid"></div>
                 <div class="content">
-                    <p class="lead"><?php echo "From left to right: "; $boe_count = count($boe); foreach($boe as $b): echo $b['firstname'] . " " . $b['lastname']; if($boe_count > 1){ echo ", "; } $boe_count--; endforeach; ?></p>
+                    <p class="lead">
+                        <?php echo "From left to right: "; ?>
+                        <?php $boe_count = count($boe); foreach($boe as $b): ?> 
+                            <a href="#" data-toggle="modal" data-target="#boe-modal" data-values='<?php echo json_encode(str_replace("'", "&apos;", $b)); ?>' onclick="boeInfo(this);"><?php echo $b['firstname'] . " " . $b['lastname'];?></a><?php if($boe_count > 1){ echo ", "; } $boe_count--; ?>
+                        <?php endforeach; ?>
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+
+    function boeInfo(boeInfo){
+        boe = $(boeInfo).data("values");
+        
+        $("#trustee-img").attr("src", 'images/contacts/' + boe['photo']);
+        $("#trustee-name").html(boe['firstname'] + " " + boe['lastname']);
+        $("#trustee-position").html(boe['position']);
+        $("#trustee-place").html(boe['position_specifics']);
+        $("#trustee-email").html(boe['email']);
+        $("#trustee-phone").html(boe['phone']);
+        $("#trustee-desc").html(boe['contact_desc']);
+
+    }
+
+</script>
