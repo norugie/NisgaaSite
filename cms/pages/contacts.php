219309885d150c2b03e724da5f6b84418ccc7b32
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-06-04 11:50:34 -0700

    added department modify function for contacts

--- cms/pages/contacts.php
+++ cms/pages/contacts.php
@@ -216,6 +216,176 @@
 </div>
 <?php } ?>
 
+<!-- Department list information - Visible only when logged in user is an Admin -->
+<?php if($_SESSION['type'] == 1){ ?>
+<div class="row clearfix">
+    <!-- FINANCE -->
+    <?php $school = $interaction->SchoolInfoPerSchool($database, 10); ?>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <h4>DEPARTMENT INFORMATION - FINANCE</h4>      
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <center>
+                            <button type="button" data-toggle="modal" data-target="#edit-school-modal" class="btn bg-green waves-effect" style="display: inline-block;" 
+                            data-values='<?php echo json_encode(str_replace("'", "&apos;", $school)); ?>' onclick="editSchool(this);"><i class="material-icons">edit</i><span>MODIFY</span></button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Name:</b> <?php echo $school['school_name']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Address:</b> <?php echo $school['school_addr']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Abbreviation:</b> <?php echo $school['school_abbv']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Email Address:</b> <?php echo $school['school_email']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Phone Number: </b> <?php echo $school['school_phone']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Head: </b> <?php echo $school['school_principal']; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- SDSS -->
+    <?php $school = $interaction->SchoolInfoPerSchool($database, 9); ?>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <h4>DEPARTMENT INFORMATION - STUDENT DATA SUPPORT SERVICES</h4>      
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <center>
+                            <button type="button" data-toggle="modal" data-target="#edit-school-modal" class="btn bg-green waves-effect" style="display: inline-block;" 
+                            data-values='<?php echo json_encode(str_replace("'", "&apos;", $school)); ?>' onclick="editSchool(this);"><i class="material-icons">edit</i><span>MODIFY</span></button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Name:</b> <?php echo $school['school_name']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Address:</b> <?php echo $school['school_addr']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Abbreviation:</b> <?php echo $school['school_abbv']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Email Address:</b> <?php echo $school['school_email']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Phone Number: </b> <?php echo $school['school_phone']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Head: </b> <?php echo $school['school_principal']; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- TECH -->
+    <?php $school = $interaction->SchoolInfoPerSchool($database, 1); ?>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <h4>DEPARTMENT INFORMATION - TECH</h4>      
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <center>
+                            <button type="button" data-toggle="modal" data-target="#edit-school-modal" class="btn bg-green waves-effect" style="display: inline-block;" 
+                            data-values='<?php echo json_encode(str_replace("'", "&apos;", $school)); ?>' onclick="editSchool(this);"><i class="material-icons">edit</i><span>MODIFY</span></button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Name:</b> <?php echo $school['school_name']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Address:</b> <?php echo $school['school_addr']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Abbreviation:</b> <?php echo $school['school_abbv']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Email Address:</b> <?php echo $school['school_email']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Phone Number: </b> <?php echo $school['school_phone']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Head: </b> <?php echo $school['school_principal']; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- MAINTENANCE -->
+    <?php $school = $interaction->SchoolInfoPerSchool($database, 7); ?>
+    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <h4>DEPARTMENT INFORMATION - MAINTENANCE</h4>      
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <center>
+                            <button type="button" data-toggle="modal" data-target="#edit-school-modal" class="btn bg-green waves-effect" style="display: inline-block;" 
+                            data-values='<?php echo json_encode(str_replace("'", "&apos;", $school)); ?>' onclick="editSchool(this);"><i class="material-icons">edit</i><span>MODIFY</span></button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="row clearfix">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Name:</b> <?php echo $school['school_name']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Address:</b> <?php echo $school['school_addr']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Abbreviation:</b> <?php echo $school['school_abbv']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Email Address:</b> <?php echo $school['school_email']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Phone Number: </b> <?php echo $school['school_phone']; ?></p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <p><b>Department Head: </b> <?php echo $school['school_principal']; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php } ?>
+
 
 <!-- Contacts -->
 
