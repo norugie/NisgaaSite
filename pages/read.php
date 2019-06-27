d2c5f3585c126b888bff384ab1e01164170c7d71
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-06-27 09:07:23 -0700

    redirect to 404 page

--- pages/read.php
+++ pages/read.php
@@ -1,9 +1,21 @@
 <?php 
     if($page_name == 'news' && isset($url[1]) && !empty($url[1]) && $url[1] == 'read' && isset($url[2]) && !empty($url[2])){
         $post_info = $site->postInformation($database, $url[2]);
-        require 'news_read.php';
+        if($post_info['status'] == 'Closed' || empty($post_info)){
+        ?>
+            <script>window.location.replace("/404");</script>
+        <?php
+        } else {
+            require 'news_read.php';
+        }
     } else if($page_name == 'careers' && isset($url[1]) && !empty($url[1]) && $url[1] == 'read' && isset($url[2]) && !empty($url[2])){
         $career = $site->jobInformation($database, $url[2]);
-        require 'career_read.php';
+        if($career['status'] == 'Closed' || empty($career)){
+        ?>
+            <script>window.location.replace("/404");</script>
+        <?php
+        } else {
+            require 'career_read.php';
+        }
     }
 ?>
