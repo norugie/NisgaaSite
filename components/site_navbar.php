20e52eb54366613ea84e56b4cc39ca475cfd4f34
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-07-04 11:49:42 -0700

    edited navbar

--- components/site_navbar.php
+++ components/site_navbar.php
@@ -51,7 +51,6 @@
                     <h5>Departments</h5>
                     <ul class="list-unstyled mb-3">
                     <li class="nav-item"><a href="/department/finance" class="nav-link">Finance</a></li>
-                    <!-- <li class="nav-item"><a href="/department/sdss" class="nav-link">Student Data Support Services</a></li> -->
                     <li class="nav-item"><a href="/department/tech" class="nav-link">Information Technology Department</a></li>
                     <li class="nav-item"><a href="/department/maintenance" class="nav-link">Transportation and Maintenance Department</a></li>
                     </ul>      
