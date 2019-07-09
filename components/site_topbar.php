5af30ce5326f03de77fa209a6dbe7065d978645b
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-07-09 14:09:34 -0700

    added separate page for topbar

---
+++ components/site_topbar.php
@@ -0,0 +1,16 @@
+<div class="top-bar">
+    <div class="row d-flex align-items-center">
+    <div class="col-md-6 d-md-block d-none">
+        <p>Contact us at <?php echo $info['school_phone']; ?> or <?php echo $info['school_email']; ?>.</p>
+    </div>
+    <div class="col-md-6">
+        <div class="d-flex justify-content-md-end justify-content-between">
+        <div class="login"><a href="https://webdev.nisgaa.bc.ca/<?php if(isset($_SESSION['id']) && isset($_SESSION['type'])){ ?>cms/<?php } else { ?>login<?php } ?>" class="login-btn"><i class="fa fa-sign-in"></i><span class="d-md-inline-block"><?php if(isset($_SESSION['id']) && isset($_SESSION['type'])){ ?>Hello, <?php echo $_SESSION['firstname']; ?><?php } else { ?>Login<?php } ?></span></a></div>
+        <ul class="social-custom list-inline">
+            <li class="list-inline-item"><a href="https://www.facebook.com/nisgaa.sd92/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li class="list-inline-item"><a href="mailto:<?php echo $info['school_email']; ?>"><i class="fa fa-envelope"></i></a></li>
+        </ul>
+        </div>
+    </div>
+    </div>
+</div>
