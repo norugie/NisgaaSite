65ae3f9fa1a453d685103aebc7b7edf7e906739d
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-07-16 09:35:46 -0700

    topbar fix

--- components/site_topbar.php
+++ components/site_topbar.php
@@ -5,7 +5,7 @@
     </div>
     <div class="col-md-6">
         <div class="d-flex justify-content-md-end justify-content-between">
-        <div class="login"><a href="https://webdev.nisgaa.bc.ca/<?php if(isset($_SESSION['id']) && isset($_SESSION['type'])){ ?>cms/<?php } else { ?>login<?php } ?>" class="login-btn"><i class="fa fa-sign-in"></i><span class="d-md-inline-block"><?php if(isset($_SESSION['id']) && isset($_SESSION['type'])){ ?>Hello, <?php echo $_SESSION['firstname']; ?><?php } else { ?>Login<?php } ?></span></a></div>
+        <div class="login"><a href="https://webdev.nisgaa.bc.ca/login" class="login-btn"><i class="fa fa-sign-in"></i><span class="d-md-inline-block">Control Panel</span></a></div>
         <ul class="social-custom list-inline">
             <li class="list-inline-item"><a href="https://www.facebook.com/nisgaa.sd92/" target="_blank"><i class="fa fa-facebook"></i></a></li>
             <li class="list-inline-item"><a href="mailto:<?php echo $info['school_email']; ?>"><i class="fa fa-envelope"></i></a></li>
