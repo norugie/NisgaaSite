fa2d5341455d980b527d6867421afa03040b82ed
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-07-16 15:15:22 -0700

    privacy policy

---
+++ pages/privacy.php
@@ -0,0 +1,31 @@
+<div class="col-md-9">
+    <div class="heading">
+        <h2>PRIVACY NOTICE</h2>
+    </div>
+    <p class="lead">This privacy notice discloses the privacy practices for <a href="https://nisgaa.bc.ca">School District No. 92</a>. This privacy notice applies solely to information collected by this website. It will notify you of the following:</p>
+    <ol>
+        <li class="lead">What personally identifiable information is collected from you through the website, how it is used and with whom it may be shared.</li>
+        <li class="lead">What choices are available to you regarding the use of your data.</li>
+        <li class="lead">The security procedures in place to protect the misuse of your information.</li>
+    </ol>
+    <div class="heading">
+        <h2>Information Collection, Use, and Sharing</h2>
+    </div>
+    <p class="lead">We are the sole owners of the information collected on this site. We only have access to/collect information that you voluntarily give us via email or other direct contact from you. We will not sell or rent this information to anyone.</p>
+    <p class="lead">We will use your information to respond to you, regarding the reason you contacted us. We will not share your information with any third party outside of our organization, other than as necessary to fulfill your request, e.g. to ship an order.</p>
+    <p class="lead">Unless you ask us not to, we may contact you via email in the future to tell you about specials, new products or services, or changes to this privacy policy.</p>
+    <p class="lead">You can do the following at any time by contacting us via the email address or phone number given on our website:</p>
+    <ul>
+        <li class="lead">See what data we have about you, if any.</li>
+        <li class="lead">Change/correct any data we have about you.</li>
+        <li class="lead">Have us delete any data we have about you.</li>
+        <li class="lead">Express any concern you have about our use of your data.</li>
+    </ul>
+    <div class="heading">
+        <h2>Security</h2>
+    </div>
+    <p class="lead">We take precautions to protect your information. When you submit sensitive information via the website, your information is protected both online and offline.</p>
+    <p class="lead">Wherever we collect sensitive information (such as credit card data), that information is encrypted and transmitted to us in a secure way. You can verify this by looking for a lock icon in the address bar and looking for "https" at the beginning of the address of the Web page.</p>
+    <p class="lead">While we use encryption to protect sensitive information transmitted online, we also protect your information offline. Only employees who need the information to perform a specific job (for example, billing or customer service) are granted access to personally identifiable information. The computers/servers in which we store personally identifiable information are kept in a secure environment.</p>
+    <p class="lead"><b>If you feel that we are not abiding by this privacy policy, you should contact us immediately via telephone at XXX YYY-ZZZZ or via email.</b></p>
+</div>
