d895ba8ce91bf80eab8a84e57bf94d9c3fcccd9f
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-07-16 14:06:00 -0700

    ignoring files

--- functions/post_options.php
+++ functions/post_options.php
@@ -158,7 +158,7 @@
                     'link' => $post_link,
                     'message' => $message
                 ];
-                $pageAccessToken = "EAAKGGWIraNYBAMD3xwrZBKMte0R4adCZAUAINJ3p2Ctat8vdCW89rdxCxCj4RAc0UIwlXDv8lQiO9Akv94c4le78RUJWtFjpZAjZCa2ZBXO5xH7EkClZBSkKdaIdnTzwqv69K7meGvXb7v2v98ZA0O0wR0g5nZCRZCj4AwX7CgNEqZCQZDZD";
+                
                 require_once('autopost_fb.php'); // Facebook Autopost Class
                 
                 try {
@@ -548,7 +548,7 @@
                     'link' => $post_link,
                     'message' => $message
                 ];
-                $pageAccessToken = "EAAKGGWIraNYBAMD3xwrZBKMte0R4adCZAUAINJ3p2Ctat8vdCW89rdxCxCj4RAc0UIwlXDv8lQiO9Akv94c4le78RUJWtFjpZAjZCa2ZBXO5xH7EkClZBSkKdaIdnTzwqv69K7meGvXb7v2v98ZA0O0wR0g5nZCRZCj4AwX7CgNEqZCQZDZD";
+                
                 require_once('autopost_fb.php'); // Facebook Autopost Class
                 
                 try {
