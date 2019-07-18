4753fe99c326510e1ba9bc8134adf2bba1a5901e
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-07-18 13:44:42 -0700

    revisions in accordance to dns change

--- components/site_topbar.php
+++ components/site_topbar.php
@@ -5,7 +5,7 @@
     </div>
     <div class="col-md-6">
         <div class="d-flex justify-content-md-end justify-content-between">
-        <div class="login"><a href="https://webdev.nisgaa.bc.ca/login" class="login-btn"><i class="fa fa-sign-in"></i><span class="d-md-inline-block">Control Panel</span></a></div>
+        <div class="login"><a href="/login" class="login-btn"><i class="fa fa-sign-in"></i><span class="d-md-inline-block">Site Panel</span></a></div>
         <ul class="social-custom list-inline">
             <li class="list-inline-item"><a href="https://www.facebook.com/nisgaa.sd92/" target="_blank"><i class="fa fa-facebook"></i></a></li>
             <li class="list-inline-item"><a href="mailto:<?php echo $info['school_email']; ?>"><i class="fa fa-envelope"></i></a></li>
--- pages/privacy.php
+++ pages/privacy.php
@@ -11,9 +11,9 @@
     <div class="heading">
         <h2>Information Collection, Use, and Sharing</h2>
     </div>
-    <p class="lead">We are the sole owners of the information collected on this site. We only have access to/collect information that you voluntarily give us via email or other direct contact from you. We will not sell or rent this information to anyone.</p>
-    <p class="lead">We will use your information to respond to you, regarding the reason you contacted us. We will not share your information with any third party outside of our organization, other than as necessary to fulfill your request, e.g. to ship an order.</p>
-    <p class="lead">Unless you ask us not to, we may contact you via email in the future to tell you about specials, new products or services, or changes to this privacy policy.</p>
+    <p class="lead">We are the sole owners of the information collected on this site. We only have access to collect information that you voluntarily give us via the website application form, email, or other direct contact from you. We will not sell or rent this information to anyone.</p>
+    <p class="lead">We will use your information to respond to you, regarding the reason you contacted us.  We will not share your information with any third party outside of our organization, other than as necessary to fulfill your request.</p>
+    <p class="lead">Unless you ask us not to, we may contact you via email in the future to tell you about the result of your requests, or changes to this privacy policy.</p>
     <p class="lead">You can do the following at any time by contacting us via the email address or phone number given on our website:</p>
     <ul>
         <li class="lead">See what data we have about you, if any.</li>
@@ -25,7 +25,7 @@
         <h2>Security</h2>
     </div>
     <p class="lead">We take precautions to protect your information. When you submit sensitive information via the website, your information is protected both online and offline.</p>
-    <p class="lead">Wherever we collect sensitive information (such as credit card data), that information is encrypted and transmitted to us in a secure way. You can verify this by looking for a lock icon in the address bar and looking for "https" at the beginning of the address of the Web page.</p>
-    <p class="lead">While we use encryption to protect sensitive information transmitted online, we also protect your information offline. Only employees who need the information to perform a specific job (for example, billing or customer service) are granted access to personally identifiable information. The computers/servers in which we store personally identifiable information are kept in a secure environment.</p>
-    <p class="lead"><b>If you feel that we are not abiding by this privacy policy, you should contact us immediately via telephone at XXX YYY-ZZZZ or via email.</b></p>
+    <p class="lead">Wherever we collect sensitive information, that information is encrypted and transmitted to us in a secure way. You can verify this by looking for a lock icon in the address bar and looking for "https" at the beginning of the address of the Web page.</p>
+    <p class="lead">While we use encryption to protect sensitive information transmitted online, we also protect your information offline. Only employees who need the information to perform your requests and assess your application are granted access to personally identifiable information. The computers/servers in which we store personally identifiable information are kept in a secure environment.</p>
+    <p class="lead"><b>If you feel that we are not abiding by this privacy policy, you should contact us immediately via telephone at +1-250-633-2228 or email us at <a href="mailto:sd92@nisgaa.bc.ca">sd92@nisgaa.bc.ca</a>.</b></p>
 </div>
