d85ec80c2209b33c28dd01ea433c605c32157dbd
Rugie-Ann Barrameda <rbarrameda@nisgaa.bc.ca>
2019-09-27 10:34:05 -0700

    remove changes

--- functions/login_options.php
+++ functions/login_options.php
@@ -9,7 +9,7 @@
         
 		$sql = "SELECT * FROM users WHERE username = '$user' AND password = md5('$pass')";
 		$query = mysqli_query($loginProcess->con, $sql);
-		if(!$query){
+		if (!$query) {
 			header("location: ../login.php?error=true");
 		} else {
 			$loginInfo = mysqli_fetch_assoc($query);
@@ -17,19 +17,7 @@
 				header("location:../login.php?invalid=true");
 			} else {
 				if($loginInfo['status'] !== 'Inactive'){
-					$uid = $loginInfo['id'];
-					$sql = "SELECT * FROM sys_groups_assignment WHERE users_id = '$uid' AND groups_id = '1'";
-					$query = mysqli_query($loginProcess->con, $sql);
-					if(!$query){
-						header("location: ../login.php?restricted=true");
-					} else {
-						$counter = mysqli_fetch_assoc($query);
-						if(count($counter) === 0){
-							header("location:../login.php?restricted=true");
-						} else {
-							$loginProcess->routerUser($loginInfo);	
-						}
-					}
+					$loginProcess->routerUser($loginInfo);
 				} else {
 					header("location: ../login.php?inactive=true");
 				}
