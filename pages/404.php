a1145c4ebdb8e72e167ce66e107234baa4070288
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-10-21 13:57:54 -0700

    error page revisions

--- pages/404.php
+++ pages/404.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div id="error-page" class="col-md-8 mx-auto text-center">
         <div class="box">
-            <p class="text-center"><a href="index.html"><img src="https://www.nisgaa.bc.ca/nisgaa-icon-banner.png" alt="Nisga'a SD92 Icon"></a></p>
+            <p class="text-center"><a href="/"><img src="https://www.nisgaa.bc.ca/nisgaa-icon-banner.png" alt="Nisga'a SD92 Icon"></a></p>
             <h3>We are sorry - this page may have been deleted or does not exist</h3>
             <h4 class="text-muted">Error 404 - Page not found</h4>
             <p class="buttons"><a href="/" class="btn btn-template-outlined"><i class="fa fa-home"></i> Go to Homepage</a></p>
--- pages/restricted.php
+++ pages/restricted.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div id="error-page" class="col-md-8 mx-auto text-center">
         <div class="box">
-            <p class="text-center"><a href="index.html"><img src="https://www.nisgaa.bc.ca/nisgaa-icon-banner.png" alt="Nisga'a SD92 Icon"></a></p>
+            <p class="text-center"><a href="/"><img src="https://www.nisgaa.bc.ca/nisgaa-icon-banner.png" alt="Nisga'a SD92 Icon"></a></p>
             <h3>You are restricted from accessing that page</h3>
             <h4 class="text-muted">Error 403 - Forbidden page</h4>
             <p class="buttons"><a href="/" class="btn btn-template-outlined"><i class="fa fa-home"></i> Go to Homepage</a></p>
