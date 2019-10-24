72f338ac0a7680f733156cf5dcb9bf486e1c804b
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2019-10-24 14:13:26 -0700

    about revisions

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,5 @@
 <?php 
     $about = $site->aboutList($database, $schoolContent); 
-    $programs = $site->programList($database, $schoolContent);
 ?>
 <div class="col-md-9">
     <!-- ABOUT US CONTENT -->
@@ -14,31 +13,5 @@
                 <p class="lead"><?php echo $about['web_desc']; ?></p>
             </div>
         </div>
-    </section>
-    
-    <!-- PROGRAMS CONTENT -->
-    <section>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="heading">
-                    <h2>PROGRAMS</h2>
-                </div>
-                <!-- Display programs for SD92 -->
-                <p class="lead"><?php echo $programs['web_desc']; ?></p>
-            </div>
-        </div>
-    </section>
-
-    <!-- VISION CONTENT -->
-    <section>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="heading">
-                    <h2>VISION FOR EDUCATION</h2>
-                </div>
-                <img class="img-responsive img-fluid" style="display: block; margin-left: auto; margin-right: auto; padding: 10px;" src="/images/site/nisgaa-vision.png" alt="">
-            </div>
-        </div>
-    </section>
-    
+    </section>    
 </div>
