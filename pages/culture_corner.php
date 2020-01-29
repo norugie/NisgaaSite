1825d0a2fc8d362e89dd4f8034c47058e3064815
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2020-01-29 15:22:39 -0800

    delete culture corner

--- pages/culture_corner.php
+++
@@ -1,6 +0,0 @@
-<?php $page_info = $site->cultureInformation($database); ?>
-<div id="blog-post" class="col-md-9">
-    <div id="post-content">
-        <?php echo $page_info['web_desc']; ?>
-    </div>
-</div>
